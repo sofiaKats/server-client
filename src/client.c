34e3c5f24805748622326e7205f2bcb31f667183
sofiaKats <sophiakatsaouni@gmail.com>
2022-05-23 17:15:42 +0300

    first prints of each file all done

--- src/client.c
+++ src/client.c
@@ -3,11 +3,14 @@
 #include "validation.h"
 
 // ./remoteClient -i 127.0.0.1 -p 12500 -d Server
+// ./remoteClient -i linux12.di.uoa.gr -p 12500 -d Server
 int main(int argc, char* argv[]) 
 {
     int server_port;
     char directory[512], server_ip[512];
     // check command line arguments
     if(!client_valid_args(argc, argv, server_ip, &server_port, directory)) exit(1);
+    printf("Client's parameters are:\n");
+    printf("serverIP: %s\nport: %d\ndirectory: %s\n", server_ip, server_port, directory);
     return 0;
 }
--- src/server.c
+++ src/server.c
@@ -8,5 +8,8 @@ int main(int argc, char* argv[])
     int port, thread_pool_size, queue_size, block_size;
     // check command line arguments
     if(!server_valid_args(argc, argv, &port, &thread_pool_size, &queue_size, &block_size)) exit(1);
+    printf("Server's parameters are: \n");
+    printf("port: %d\nthread_pool_size: %d\nqueue_size: %d\nblock_size: %d\n", port, thread_pool_size, queue_size, block_size);
+    
     return 0;
 }
