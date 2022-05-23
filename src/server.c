acf608ea2cab82d5e26d97d5dc952058a004084f
sofiaKats <sophiakatsaouni@gmail.com>
2022-05-23 16:13:35 +0300

    command line args check in server all done

--- src/server.c
+++ src/server.c
@@ -1,9 +1,14 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
-int check_args(int argc, char** argv);
+int server_valid_args(int argc, char **argv, int *port, int *thread_pool_size, int *queue_size, int *block_size);
 
 int main(int argc, char* argv[]) 
 {
+    int port, thread_pool_size, queue_size, block_size;
+    // check command line arguments
+    if(!server_valid_args(argc, argv, &port, &thread_pool_size, &queue_size, &block_size)) exit(1);
     return 0;
 }
 
@@ -13,3 +18,30 @@ int main(int argc, char* argv[])
 // 2. thread pool size: Ο αριθμός των worker threads στο thread pool.
 // 3. queue size: Ο αριθμός θέσεων στην ουρά εκτέλεσης.
 // 4. block size: Το μέγεθος των μπλοκ των αρχείων σε bytes που θα στέλνουν οι worker threads.
+
+int server_valid_args(int argc, char **argv, int *port, int *thread_pool_size, int *queue_size, int *block_size)
+{
+    if (argc != 9) {
+        printf("Please give exactly 9 arguments.\n\n");
+        return 0;
+    }
+    
+    for (int i = 1; i < argc; ++i)
+    {
+        if (!strcmp(argv[i], "-p")) 
+            *port = atoi(argv[++i]);
+        else if (!strcmp(argv[i], "-s"))
+            *thread_pool_size = atoi(argv[++i]);
+        else if (!strcmp(argv[i], "-q"))
+            *queue_size = atoi(argv[++i]);
+        else if (!strcmp(argv[i], "-b"))
+            *block_size = atoi(argv[++i]);
+        else
+        {
+            printf("Invalid command line argument option given: %s \n\n\n",argv[i]);
+            return 0;
+        }
+    }
+
+    return 1; // all arguments are ok
+}
