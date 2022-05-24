290afa246a89b62caaf2124667d905b4b53eecd9
sofiaKats <sophiakatsaouni@gmail.com>
2022-05-24 19:16:40 +0300

    directory readiong from client all done

--- src/client.c
+++ src/client.c
@@ -25,7 +25,7 @@ int main(int argc, char* argv[])
     printf("serverIP: %s\nport: %d\ndirectory: %s\n", server_ip, server_port, directory);
 
     int sock, i;
-    char buf[256];
+    //char buf[256];
     struct sockaddr_in server;
     struct sockaddr *serverptr = (struct sockaddr*)&server;
     struct hostent *rem;
@@ -49,21 +49,21 @@ int main(int argc, char* argv[])
 	   perror_exit("connect @ client.c line 46\n");
     printf("Connecting to %s on port %d\n", server_ip, server_port);
 
-     do 
-     {
-    	printf("Give input string: ");
-    	fgets(buf, sizeof(buf), stdin);	/* Read from stdin*/
-    	for(i=0; buf[i] != '\0'; i++) { /* For every char */
+    //  do 
+    //  {
+    	//printf("Give input string: ");
+    	//fgets(buf, sizeof(buf), stdin);	/* Read from stdin*/
+    	for(i=0; directory[i] != '\0'; i++) { /* For every char */
     	    /* Send i-th character */
-        	if (write(sock, buf + i, 1) < 0)
+        	if (write(sock, directory + i, 1) < 0)
         	   perror_exit("write");
             /* receive i-th character transformed */
-        	if (read(sock, buf + i, 1) < 0)
-        	    perror_exit("read");   
+        	// if (read(sock, buf + i, 1) < 0)
+        	//     perror_exit("read");   
     	}
-    	printf("Received string: %s", buf);
+    	//printf("Received string: %s", buf);
 
-    } while (strcmp(buf, "END\n") != 0); /* Finish on "end" */
+    //} while (strcmp(buf, "END\n") != 0); /* Finish on "end" */
     
     close(sock);                 /* Close socket and exit */
     return 0;
--- src/server_functs.c
+++ src/server_functs.c
@@ -4,15 +4,16 @@ void * receive_dir_name(void *argp) {
     struct thread_funct_args *args = (struct thread_funct_args*) argp; // coping passed argument struct
     char buf[1];
     int err;
+    printf("Given directiry name is:\n");
     while(read(args->newsock, buf, 1) > 0) {  /* Receive 1 char */
     	putchar(buf[0]);           /* Print received char */
     	/* Capitalize character */
-    	buf[0] = toupper(buf[0]);
+    	//buf[0] = toupper(buf[0]);
     	/* Reply */
-    	if (write(args->newsock, buf, 1) < 0)
-            fprintf(stderr, "write @ child_server line 84\n");
+    	// if (write(args->newsock, buf, 1) < 0)
+        //     fprintf(stderr, "write @ child_server line 84\n");
     }
-    printf("Closing connection.\n");
+    printf("\nClosing connection.\n");
     close(args->newsock);	  /* Close socket */
     pthread_exit(NULL); 
 }
